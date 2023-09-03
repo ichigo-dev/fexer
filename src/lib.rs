f851a1acae71090f5a9fad3d1e319fa3d5d57b18
ichigo <dev.honda.ichigo@gmail.com>
2023-09-04 08:49:27 +0900

    test: Add a runtime functionality test

--- src/lib.rs
+++ src/lib.rs
@@ -15,3 +15,28 @@ mod task_queue;
 mod waker;
 mod executor;
 mod utils;
+
+
+#[cfg(test)]
+mod tests
+{
+    use crate::executor::Executor;
+    use crate::utils::Result;
+
+    use std::thread;
+    use std::time::Duration;
+
+    //--------------------------------------------------------------------------
+    //  Tests the Executor.
+    //--------------------------------------------------------------------------
+    #[test]
+    fn run_async_function()
+    {
+        let mut executor = Executor::new();
+        executor.spawn(async
+        {
+            println!("Hello from async function!");
+        });
+        executor.run().unwrap();
+    }
+}
